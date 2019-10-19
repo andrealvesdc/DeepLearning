da8f52992dd9fb6cbe4b972fc146961b8c3353dc
André Alves <andrealvesdc@gmail.com>
2019-10-18 21:55:03 -0300

    Recomendações com boltzmann
    Recomendações com boltzmann

---
+++ recomendacao_Boltzmann.py
@@ -0,0 +1,35 @@
+from rbm import RBM
+import numpy as np
+
+rbm = RBM(num_visible = 6, num_hidden = 2)
+
+
+base = np.array([[1,0,1,0,1,0],
+                 [1,0,1,1,0,0],
+                 [0,1,0,1,1,0],
+                 [0,0,1,1,1,1],
+                 [0,0,1,1,0,1],
+                 [1,0,0,1,0,1]])
+
+filmes = ["Blade Runner", "Logan", "O chamado",
+          "It", "A freira", "Dunkirk"]
+
+
+rbm.train(base, max_epochs=5000)
+rbm.weights
+
+usuario1 = np.array([[1,1,0,1,0,0]])
+usuario2 = np.array([[0,0,0,1,1,0]])
+
+
+rbm.run_visible(usuario1)
+rbm.run_visible(usuario2)
+
+camada_escondida = np.array([[1,0]])
+recomendacao = rbm.run_hidden(camada_escondida)
+
+
+for i in range(len(usuario1[0])):
+    #print(usuario1[0,i])
+    if usuario2[0,i] == 0 and recomendacao[0,i] == 1:
+        print(filmes[i])
